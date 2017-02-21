39fb367301267024e5533cb7e7751bcd9b176329
lamfire <52089489@qq.com>
2017-02-21 11:25:07 +0800

    add more options setting support

---
+++ src/main/java/com/lamfire/warden/Version.java
@@ -0,0 +1,9 @@
+package com.lamfire.warden;
+
+/**
+ * Created by linfan on 2017/2/21.
+ */
+public class Version {
+    public static final String VERSION_NAME = "2.0.7";
+    public static final int VERSION_CODE = 27;
+}
